cb6578e40d00ddb571767afc7aa1806670f1afee
mhdhakeym <mhdhakeym786@gmail.com>
2021-10-04 22:05:35 +0800

    Updates

---
+++ precache-manifest.07ff862042e826a5a8f87ba68a0e7bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5062d282880ba84b47967051b9432f3",
+    "url": "/Homework/index.html"
+  },
+  {
+    "revision": "9e55c58bd4e5b58b52f5",
+    "url": "/Homework/static/css/main.f35f9068.chunk.css"
+  },
+  {
+    "revision": "2eadcdfca1b804953ec8",
+    "url": "/Homework/static/js/2.ea1dabc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Homework/static/js/2.ea1dabc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e55c58bd4e5b58b52f5",
+    "url": "/Homework/static/js/main.e8e43d35.chunk.js"
+  },
+  {
+    "revision": "89dfdb9ad60df23ba5cf",
+    "url": "/Homework/static/js/runtime-main.4855d849.js"
+  }
+]);
